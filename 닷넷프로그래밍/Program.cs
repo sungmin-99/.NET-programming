ffb7865ade1a1aaee82406c278d73db6a804cbe6
HNU <HNU@DESKTOP-8C4Q3SL>
2023-03-07 14:28:27 +0900

    초기 커밋.

---
+++ 닷넷프로그래밍/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace 닷넷프로그래밍
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write");
+            Console.WriteLine("WriteLine");
+
+            Console.WriteLine("WriteLine");
+            Console.WriteLine("WriteLine");
+
+        }
+    }
+}
