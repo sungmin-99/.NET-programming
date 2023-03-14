715bf7e05a706111dc19f0ee88e59171123133bc
Sungmin <20180595@gm.hannam.ac.kr>
2023-03-14 14:32:21 +0900

    init commit

---
+++ 닷넷프로그래밍/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        var number = Console.ReadLine();
+        if (int.Parse(number) == 0)
+        {
+            Console.WriteLine("0 입니다.");
+        }
+        else if (int.Parse(number) < 0)
+        {
+            Console.WriteLine("음수 입니다.");
+        }
+        else
+        {
+            Console.WriteLine("양수 입니다.");
+        }
+    }
+}
---
+++ 닷넷프로그래밍/Class2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        List<int> list = new List<int>();
+
+        while (true)
+        {
+            var tmp = int.Parse(Console.ReadLine());
+            if (tmp == 0)
+            {
+                break;
+            }
+            if (!(list.Contains(tmp)))
+            {
+                list.Add(tmp);
+            }
+        }
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.Write(list[i]);
+        }
+    }
+}
