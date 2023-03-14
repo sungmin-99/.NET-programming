58515cd4bcde0d3eb158165c51ff22cada00adc3
Sungmin <20180595@gm.hannam.ac.kr>
2023-03-14 14:27:10 +0900

    init commit

--- 닷넷프로그래밍/Program.cs
+++ 닷넷프로그래밍/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace 닷넷프로그래밍
 {
@@ -6,12 +8,29 @@ namespace 닷넷프로그래밍
     {
         static void Main(string[] args)
         {
-            Console.Write("Write");
-            Console.WriteLine("WriteLine");
+            var word = Console.ReadLine();
+            var ansWord = "";
 
-            Console.WriteLine("WriteLine");
-            Console.WriteLine("WriteLine");
 
+            List<int> ansCount = new List<int>();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (!(ansWord.Contains(word[i])))
+                {
+                    ansWord = ansWord + word[i];
+                    ansCount.Add(1);
+                }
+                else
+                {
+                    ansCount[ansWord.IndexOf(word[i])] += 1;
+                }
+            }
+
+            var ansMax = ansCount.Max();     // count중 가장 높은 수
+            var maxCount = ansCount.IndexOf(ansMax); // 가장 높은 수의 index
+            Console.WriteLine(ansMax);
+            Console.WriteLine(ansWord[maxCount]);
         }
     }
 }
